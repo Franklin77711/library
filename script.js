28338a981d794561b58c8036cad2991f36df4962
Franklin77711 <robi1000350@gmail.com>
2022-12-12 11:35:56 +0100

    added error handle, better design

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const popBtn = document.getElementById("pop-btn");
 const addBtn = document.getElementById("add-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const popform = document.getElementById("form");
+const errorDiv = document.getElementById("error");
 
 let games = [];
 
@@ -29,14 +30,14 @@ function addToLibrary(){
         lname.textContent = "Game Name: "
         card.appendChild(lname);
         const tname = document.createElement("p");
-        tname.setAttribute("class", "name");
+        tname.setAttribute("class", "name answer");
         tname.textContent = games[games.length-1].name;
         card.appendChild(tname);
         const lplaytime = document.createElement("label");
         lplaytime.textContent = "Hours played: "
         card.appendChild(lplaytime);
         const tplaytime = document.createElement("p");
-        tplaytime.setAttribute("class", "playtime");
+        tplaytime.setAttribute("class", "playtime answer");
         tplaytime.textContent = games[games.length-1].playtime;
         card.appendChild(tplaytime);
         const ldifficulty = document.createElement("label");
@@ -44,7 +45,7 @@ function addToLibrary(){
         card.appendChild(ldifficulty);
         const tdifficulty = document.createElement("p");
         tdifficulty.textContent = games[games.length-1].difficulty;
-        tdifficulty.setAttribute("class", "difficulty");
+        tdifficulty.setAttribute("class", "difficulty answer");
         card.appendChild(tdifficulty);
         //switch add
         const lplatinum = document.createElement("label");
@@ -62,17 +63,35 @@ function addToLibrary(){
         
     };
     function popclose(){
-        gname.textContent="";
-        playtime.textContent="";
-        difficulty.textContent="";
+        gname.value="";
+        playtime.value="";
+        difficulty.value="";
         popform.style.visibility = "hidden";
+        errorDiv.textContent = "";
         
     }
 
-    function magicHappens(){
-        addToLibrary();
-        createCard();
-        popclose();
+function magicHappens(){
+    let falsecheck = false;
+    //error handle for same game add
+        for(let nextgame of games){
+            if(nextgame.name == gname.value){
+                errorDiv.textContent = "This game has already been added";
+                falsecheck=true;
+            }
+        }
+         //error handle for difficulty must be 0-10
+        if (difficulty.value > 10 || difficulty.value < 0){
+            errorDiv.textContent = "Difficulty must be between 10 and 0";
+            falsecheck=true;
+        }
+        else if(falsecheck == false){
+                addToLibrary();
+                createCard();
+                popclose();
+                falsecheck = true;
+                console.log(games)
+            }
     }
 
 //buttonclick events
